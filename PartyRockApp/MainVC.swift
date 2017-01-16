57e7f5e42c86c4b78f28340939a7bc439cbda34d
Dragoslav Ignjatovic <draza021@gmail.com>
2017-01-16 14:53:26 +0100

    completed layout

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Drago on 1/16/17.
+//  Copyright © 2017 Novotek. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    
+
+
+}
+
