734fa77e80897c3c5787dfe857f225eedeaffafc
Dragoslav Ignjatovic <draza021@gmail.com>
2017-01-25 17:09:38 +0100

    completed partyrock app

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -19,11 +19,22 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         let URLTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CdLhdrNgGu4\" frameborder=\"0\" allowfullscreen></iframe>"
         
         let imageURLTest = "http://oi66.tinypic.com/2cfqfib.jpg"
+        let imageURLTest1 = "http://oi64.tinypic.com/jjsl80.jpg"
+        let imageURLTest2 = "http://oi68.tinypic.com/jjtmwj.jpg"
+        let imageURLTest3 = "http://oi67.tinypic.com/2cfqfq9.jpg"
+        let imageURLTest4 = "http://oi41.tinypic.com/2nimosw.jpg"
         
         let p1 = PartyRock(imageURL: imageURLTest, videoURL: URLTest, videoTitle: "Where the Sun goes")
+        let p2 = PartyRock(imageURL: imageURLTest1, videoURL: URLTest, videoTitle: "Where the Sun goes")
+        let p3 = PartyRock(imageURL: imageURLTest2, videoURL: URLTest, videoTitle: "Where the Sun goes")
+        let p4 = PartyRock(imageURL: imageURLTest3, videoURL: URLTest, videoTitle: "Where the Sun goes")
+        let p5 = PartyRock(imageURL: imageURLTest4, videoURL: URLTest, videoTitle: "Where the Sun goes")
         
         partyRocks.append(p1)
-        
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -32,7 +43,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: <#T##IndexPath#>) as? PartyCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyCell {
             let partyRock = partyRocks[indexPath.row]
             
             cell.updateUI(partyRock: partyRock)
@@ -46,8 +57,21 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
         return partyRocks.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row]
+        
+        performSegue(withIdentifier: "VideoVC", sender: partyRock)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? VideoVC {
+            if let party = sender as? PartyRock {
+                destination.partyRock = party
+            }
+        }
     }
 
 
--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -21,7 +21,21 @@ class PartyCell: UITableViewCell {
 
     func updateUI(partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
-        //todo set image from URL
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+                
+            } catch {
+                //handle the error
+            }
+        }
+        
     }
 
 }
